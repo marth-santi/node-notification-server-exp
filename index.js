31e589d3e738d526762bbedaac8cd4e1ba407eba
artemesan <artesuto@gmail.com>
2020-06-11 19:31:47 +0700

    minor change

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://node-notification-exp.herokuapp.com:8980/');
+});
+
 app.get('/api', (req, res) => {
   res.end('Hello');
 });
