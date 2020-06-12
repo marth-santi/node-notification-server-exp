c621bcd45ec85e4e7d25c3542367e3eef8564a59
artemesan <artesuto@gmail.com>
2020-06-12 07:58:34 +0700

    commit

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://node-notification-exp.herokuapp.com:8980/');
+  res.header('Access-Control-Allow-Origin', '*');
 });
 
 app.get('/api', (req, res) => {
   res.end('Hello');
 });
 
-app.listen(8980, () => {
+app.listen(80, () => {
   console.log('Listening at 8980');
 });
